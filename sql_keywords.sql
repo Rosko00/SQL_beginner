a2bcca38322a2d3c5e30a8982db8c98cdbf45dc4
Marián Gálik <marian.galik@gmail.com>
2022-10-22 14:24:16 +0200

    Update sql_keywords.sql

--- sql_keywords.sql
+++ sql_keywords.sql
@@ -15,7 +15,7 @@ WHERE ProductID = ALL (SELECT ProductID FROM OrderDetails WHERE Quantity = 10);
 
 /* Adds, deletes, or modifies columns in a table, or changes the data type of a column in a table
 Pridáva, odstraňuje alebo upravuje stĺpce v tabuľke alebo mení typ údajov stĺpca v tabuľke */
-ALTER TABLE Customers
+ALTER TABLE my_data
 ADD Email varchar(255);
 /*The following SQL deletes the "Email" column from the "Customers" table:
 (Nasledujúci SQL vymaže stĺpec "E-mail" z tabuľky "Zákazníci":)*/
@@ -26,14 +26,14 @@ DROP COLUMN Email;
 named "BirthDate" in the "Employees" table to type year:
 (Nasledujúci SQL zmení typ údajov stĺpca
 s názvom "Dátum narodenia" v tabuľke "Zamestnanci" a zadajte rok:)*/
-ALTER TABLE Employees
+ALTER TABLE my_data
 ALTER COLUMN BirthDate year;
 
 /*Adds, deletes, or modifies columns in a table. 
 (Pridáva, odstraňuje alebo upravuje stĺpce v tabuľke) */
 /*The following SQL adds an "Email" column to the "Customers" table:
 (Nasledujúci SQL pridá stĺpec „E-mail“ do tabuľky „Zákazníci“:)*/
-ALTER TABLE Customers
+ALTER TABLE my_data
 ADD Email varchar(255);
 /*The following SQL deletes the "Email" column from the "Customers" table:
 (Nasledujúci SQL vymaže stĺpec "E-mail" z tabuľky "Zákazníci":)*/
@@ -102,3 +102,177 @@ WHERE c.CustomerName="Around the Horn" AND c.CustomerID=o.CustomerID;
 SELECT * FROM Customers
 ORDER BY CustomerName ASC; 
 
+/*Creates a back up of an existing database
+(Vytvorí zálohu existujúcej databázy)*/
+BACKUP DATABASE testDB
+TO DISK = 'D:\backups\testDB.bak'; 
+/*A differential back up only backs up the parts of the database 
+that have changed since the last full database backup.
+(Rozdielové zálohovanie zálohuje iba časti databázy, 
+ktoré sa zmenili od poslednej úplnej zálohy databázy.)*/
+BACKUP DATABASE testDB
+TO DISK = 'D:\backups\testDB.bak'
+WITH DIFFERENTIAL; 
+
+/*Selects values within a given range
+(Vyberie hodnoty v rámci daného rozsahu)*/
+/*The following SQL statement selects all products with a price BETWEEN 10 and 20:
+(Nasledujúci príkaz SQL vyberie všetky produkty s cenou 10 až 20:)*/
+SELECT * FROM Products
+WHERE Price BETWEEN 10 AND 20;
+/*To display the products outside the range of the previous example, use NOT BETWEEN:
+(Ak chcete zobraziť produkty mimo rozsahu predchádzajúceho príkladu, použite NOT BETWEEN:)*/
+SELECT * FROM Products
+WHERE Price NOT BETWEEN 10 AND 20;
+/*The following SQL statement selects all products with a ProductName BETWEEN 
+'Carnarvon Tigers' and 'Mozzarella di Giovanni':
+(Nasledujúci príkaz SQL vyberie všetky produkty s názvom produktu MEDZI 
+'Carnarvon Tigers' a 'Mozzarella di Giovanni':)*/
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
+ORDER BY ProductName;
+
+/*Creates different outputs based on conditions
+(Vytvára rôzne výstupy na základe podmienok)*/
+/*The following SQL goes through several conditions and returns a value when the specified condition is met:
+(Nasledujúci SQL prechádza niekoľkými podmienkami a vracia hodnotu, keď je zadaná podmienka splnená:)*/
+SELECT OrderID, Quantity,
+CASE
+    WHEN Quantity > 30 THEN 'The quantity is greater than 30'
+    WHEN Quantity = 30 THEN 'The quantity is 30'
+    ELSE 'The quantity is under 30'
+END
+FROM OrderDetails; 
+/*The following SQL will order the customers by City. However, if City is NULL, then order by Country:
+(Nasledujúce SQL zoradí zákazníkov podľa mesta. Ak je však Mesto NULL, zoraďte podľa krajiny:)*/
+SELECT CustomerName, City, Country
+FROM Customers
+ORDER BY
+(CASE
+    WHEN City IS NULL THEN Country
+    ELSE City
+END); 
+
+/*A constraint that limits the value that can be placed in a column
+(Obmedzenie, ktoré obmedzuje hodnotu, ktorú možno umiestniť do stĺpca)*/
+/*The following SQL creates a CHECK constraint on the "Age" column when the "Persons"
+table is created. The CHECK constraint ensures that you can not have any person below 18 years:
+(Nasledujúci SQL vytvára obmedzenie CHECK v stĺpci "Vek" pri vytváraní tabuľky "Osoby". 
+Obmedzenie CHECK zaisťuje, že nemôžete mať žiadnu osobu mladšiu ako 18 rokov:)*/
+CREATE TABLE Persons (
+    Age int,
+    CHECK (Age>=18)
+); 
+/*To create a CHECK constraint on the "Age" column when the table is already created, use the following SQL:
+(Ak chcete vytvoriť obmedzenie CHECK v stĺpci "Vek", keď je tabuľka už vytvorená, použite nasledujúci SQL:)*/
+ALTER TABLE Persons
+ADD CHECK (Age>=18); 
+/*To drop a CHECK constraint, use the following SQL:
+(Ak chcete zrušiť obmedzenie CHECK, použite nasledujúci SQL:)*/
+ALTER TABLE Persons
+DROP CHECK CHK_PersonAge; 
+
+/*Changes the data type of a column or deletes a column in a table
+(Zmení typ údajov stĺpca alebo odstráni stĺpec v tabuľke)*/
+/*ALTER COLUMN
+(ZMENIŤ STĹPEC)*/
+ALTER TABLE Employees
+ALTER COLUMN BirthDate year;
+/*DROP COLUMN
+(ODSTRÁNI STĹPEC)*/
+ALTER TABLE Customers
+DROP COLUMN ContactName;
+
+/*Adds or deletes a constraint / ADD CONSTRAINT
+(Pridá alebo odstráni obmedzenie) / PRIDAŤ OBMEDZENIE*/
+ALTER TABLE Persons
+ADD CONSTRAINT PK_Person PRIMARY KEY (ID,LastName);
+/*DROP CONSTRAINT
+(ODSTRÁNI OBMEDZENIA)*/
+ALTER TABLE Persons
+DROP INDEX UC_Person; 
+/*DROP a PRIMARY KEY Constraint
+(ZRUŠÍ obmedzenie PRIMÁRNEHO KĽÚČA)*/
+ALTER TABLE Persons
+DROP PRIMARY KEY; 
+/*DROP a FOREIGN KEY Constraint
+(ZRUŠÍ INÉ KĽÚČOVÉ obmedzenie)*/
+ALTER TABLE Orders
+DROP FOREIGN KEY FK_PersonOrder;
+/*DROP a CHECK Constraint
+(ZRUŠÍ OBMEDZENIE CHECK)*/
+ALTER TABLE Persons
+DROP CHECK CHK_PersonAge; 
+
+/*Creates a database, index, view, table, or procedure
+(Vytvorí databázu, index, zobrazenie, tabuľku alebo procedúru)*/
+/*The following SQL creates a database called "testDB":
+(Nasledujúci SQL vytvorí databázu s názvom "testDB":)*/
+ CREATE DATABASE testDB; 
+/*The CREATE TABLE command creates a new table in the database.
+(Príkaz CREATE TABLE vytvorí novú tabuľku v databáze.)*/
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+/*The following SQL creates a new table called "TestTables" 
+(which is a copy of two columns of the "Customers" table): 
+(Nasledujúci SQL vytvorí novú tabuľku s názvom "TestTables" 
+(čo je kópia dvoch stĺpcov tabuľky "Customers"):)*/
+CREATE TABLE TestTable AS
+SELECT customername, contactname
+FROM customers; 
+/*The CREATE INDEX command is used to create indexes in tables (allows duplicate values).
+(Príkaz CREATE INDEX sa používa na vytváranie indexov v tabuľkách (umožňuje duplicitné hodnoty).)*/
+CREATE INDEX idx_lastname
+ON Persons (LastName); 
+/*adn*/
+CREATE INDEX idx_pname
+ON Persons (LastName, FirstName); 
+/*The CREATE UNIQUE INDEX command creates a unique index on a table 
+(no duplicate values allowed)
+(Príkaz CREATE UNIQUE INDEX vytvorí jedinečný index v tabuľke 
+(nie sú povolené žiadne duplicitné hodnoty))*/
+CREATE UNIQUE INDEX uidx_pid
+ON Persons (PersonID); 
+/*The CREATE VIEW command creates a view.
+(Príkaz CREATE VIEW vytvorí pohľad.)*/
+CREATE VIEW [Brazil Customers] AS
+SELECT CustomerName, ContactName
+FROM Customers
+WHERE Country = "Brazil"; 
+/*The CREATE OR REPLACE VIEW command updates a view.
+(The CREATE OR REPLACE VIEW command updates a view.)*/
+CREATE OR REPLACE VIEW [Brazil Customers] AS
+SELECT CustomerName, ContactName, City
+FROM Customers
+WHERE Country = "Brazil";
+/*We can query the view above as follows:
+(Na zobrazenie vyššie môžeme dopytovať takto:)*/
+ SELECT * FROM [Brazil Customers]; 
+/*The CREATE PROCEDURE command is used to create a stored procedure.
+(Príkaz CREATE PROCEDURE sa používa na vytvorenie uloženej procedúry.)*/
+CREATE PROCEDURE SelectAllCustomers
+AS
+SELECT * FROM Customers
+GO;
+
+/*Creates a new SQL database
+(Vytvorí novú SQL databázu)*/
+ CREATE DATABASE testDB; 
+
+/*Creates an index on a table (allows duplicate values)
+(Vytvorí index v tabuľke (umožňuje duplicitné hodnoty))*/
+/*The following SQL creates an index named "idx_lastname" on the "LastName" column in the "Persons" table:
+(Nasledujúci SQL vytvorí index s názvom "idx_lastname" v stĺpci "LastName" v tabuľke "Persons":)*/
+CREATE INDEX idx_lastname
+ON Persons (LastName); 
+/*If you want to create an index on a combination of columns, 
+you can list the column names within the parentheses, separated by commas:
+(Ak chcete vytvoriť index pre kombináciu stĺpcov, 
+názvy stĺpcov môžete uviesť v zátvorkách oddelených čiarkami:)*/
+CREATE INDEX idx_pname
+ON Persons (LastName, FirstName);
